d1ba4722df7212d368bdfa4caa675c819716fc5d
F4r3d258 <farloncq07@gmail.com>
2026-01-11 13:29:27 -0600

    prueva test

--- MusicBox.Tests/obj/Debug/net10.0-windows/MusicBox.Tests.AssemblyInfo.cs
+++ MusicBox.Tests/obj/Debug/net10.0-windows/MusicBox.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MusicBox.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fe3aac455de16267ca34e4fa3749fd7e2ec241c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1050dc1696a7e36e66c4096cb00f760a7f2b22b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MusicBox.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MusicBox.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
